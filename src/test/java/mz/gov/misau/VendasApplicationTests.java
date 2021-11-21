1b655dcae03854b94600ca0c2cb5c77b2b132cae
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2021-11-21 10:43:59 +0200

    Commit do inicio do projecto de Vendas Online

---
+++ src/test/java/mz/gov/misau/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.gov.misau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
